1aeee9444fce68b029300527a988116de27a07ca
Sam Rushing <gitsam@rushing.nightmare.com>
2012-03-14 22:00:09 -0700

    stub_resolver.inflight: semaphore to control the number of inflight requests gethostbyname: only return an answer if it's the right type

--- coro/dns/stub_resolver.py
+++ coro/dns/stub_resolver.py
@@ -11,12 +11,14 @@ class QueryFailed (Exception):
 
 class stub_resolver:
 
-    def __init__ (self, nameservers):
+    def __init__ (self, nameservers, inflight=200):
         self.nameservers = nameservers
+        self.inflight = coro.semaphore (inflight)
 
     def lookup (self, qname, qtype, timeout=10, retries=3):
         m = packet.Packer()
         h = packet.Header()
+        # XXX need to avoid collisions
         h.id = random.randrange (65536)
         h.opcode = packet.OPCODE.QUERY
         h.rd = 1
@@ -26,24 +28,29 @@ class stub_resolver:
         p = m.getbuf()
         for addr in self.nameservers:
             for i in range (retries):
-                s = coro.udp_sock()
-                s.connect ((addr, 53))
-                s.send (p)
+                self.inflight.acquire (1)
                 try:
-                    reply = coro.with_timeout (timeout, s.recv, 1000)
-                    u = packet.Unpacker (reply)
-                    return u.unpack()
-                except coro.TimeoutError:
-                    pass
+                    s = coro.udp_sock()
+                    s.connect ((addr, 53))
+                    s.send (p)
+                    try:
+                        reply = coro.with_timeout (timeout, s.recv, 1000)
+                        u = packet.Unpacker (reply)
+                        return u.unpack()
+                    except coro.TimeoutError:
+                        pass
+                finally:
+                    self.inflight.release (1)
         raise QueryFailed ("no reply from nameservers")
 
     def gethostbyname (self, name, qtype):
         header, qdl, anl, nsl, arl = self.lookup (name, qtype)
-        if not anl:
-            raise QueryFailed ("no answer in nameserver reply")
+        for answer in anl:
+            name, rtype, _, ttl, addr = answer
+            if getattr (packet.TYPE, rtype) == qtype:
+                return addr
         else:
-            name, _, _, ttl, addr = random.choice (anl)
-            return addr
+            raise QueryFailed ("no answer in nameserver reply")
 
     def resolve_ipv4 (self, name):
         return self.gethostbyname (name, packet.TYPE.A)
