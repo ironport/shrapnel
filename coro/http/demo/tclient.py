2d4da49235aedcd98cf7b6d11e20e4a18cb2b2f8
Sam Rushing <gitsam@rushing.nightmare.com>
2012-03-27 16:20:15 -0700

    demo pipelined client, two different ways

---
+++ coro/http/demo/tclient.py
@@ -0,0 +1,27 @@
+# -*- Mode: Python -*-
+
+import coro
+W = coro.write_stderr
+
+from coro.httpd.client import client as http_client
+
+def t0():
+    c = http_client ('127.0.0.1', 80)
+    l = [ c.send_request ('GET', '/postgresql/html/', {}, content=None, force=True) for x in range (10) ]
+    for req in l:
+        req.wait()
+        W ('%s\n' % (req.response,))
+
+def t1():
+    c = http_client ('127.0.0.1', 80)
+    rl = coro.in_parallel ([(c.GET, ('/postgresql/html/',))] * 10)
+    for x in rl:
+        W ('%s\n' % (x.response,))
+    return rl
+
+if __name__ == '__main__':
+    import coro.backdoor
+    coro.spawn (t0)
+    coro.spawn (coro.backdoor.serve, unix_path='/tmp/xx.bd')
+    coro.event_loop()
+
