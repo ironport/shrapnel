b7ac9de4ac277215c06bfdd3df05854f284d327d
Sam Rushing <gitsam@rushing.nightmare.com>
2012-07-08 23:49:36 -0700

    setup.py for cython quadtree module

---
+++ coro/http/demo/websocket/field/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+setup (
+    name='quadtree',
+    description='Quad Tree - Spatial Search Data Structure',
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [Extension ("quadtree", ["quadtree.pyx"],)]
+    )
