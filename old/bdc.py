7cbfce9092aa5f3eedcf2d3517f271c1064a176e
Rob Vaterlaus <rob@21e6.com>
2014-12-22 13:25:39 -0800

    Fix some readline issues with backdoor client
    - python prompt was confusing some readline commands
    - save/restore readline history

--- old/bdc.py
+++ old/bdc.py
@@ -1,66 +1,80 @@
-# telnet client
-#
-# Usage : telnetclient.py hostname portnumber
-#
-# Programmed by Gang Seong Lee
-# 2000.3.20
+#!/usr/bin/env python
+
+# originally based on a telnet client by Gang Seong Lee, 2000.3.20.
 
 from telnetlib import Telnet
-import time
+
+import socket
 import sys
-from threading import *
-# import readline
-import editline
-editline.parse_and_bind ('bind -e')
+import readline
 import os
 
-host = ''
-port = 23
+class UnixTelnet (Telnet):
+    def open (self, addr, timeout=socket._GLOBAL_DEFAULT_TIMEOUT):
+        self.eof = 0
+        if type(addr) is str:
+            self.host = 'none'
+            self.port = 0
+            self.sock = socket.socket (socket.AF_UNIX, socket.SOCK_STREAM)
+        else:
+            self.host, self.port = addr
+            self.sock = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect (addr)
 
-class ReaderThread(Thread):
-    def __init__(self, telnet):
-        self.telnet = telnet
-        Thread.__init__(self)
 
-    def run(self):
-        while True:
-            str = self.telnet.read_some()
-            if str == '':
-                break
-            sys.stdout.write(str)
-            sys.stdout.flush()
+PYTHON_PROMPT = '>>> '
+PYTHON_PROMPT_REGEX = '>>> \Z'
+PYTHON_CONTINUATION_PROMPT = '... '
+PYTHON_CONTINUATION_PROMPT_REGEX = '\\.\\.\\. \Z'
+PROMPTS = [PYTHON_PROMPT, PYTHON_CONTINUATION_PROMPT]
+PROMPT_REGEXS = [PYTHON_PROMPT_REGEX, PYTHON_CONTINUATION_PROMPT_REGEX]
+HOME_DIRECTORY = os.path.expanduser("~")
+HISTORY_FILE_NAME = '.bdc_history'
 
-def main(host, port):
-    telnet = Telnet()
-    telnet.open(host, port)
+def main (addr):
+    history_file = os.path.join(HOME_DIRECTORY, HISTORY_FILE_NAME)
+    try:
+        readline.read_history_file(history_file)
+    except IOError:
+        print 'Error reading history file:', history_file
+    import atexit
+    atexit.register(readline.write_history_file, history_file)
 
-    reader = ReaderThread(telnet)
-    reader.start()
+    telnet = UnixTelnet()
+    telnet.open (addr)
 
+    # The prompt that shrapnel includes in the output interferes with
+    # readline's prompt so strip off the prompt from the output
+    # before emitting the banner text from shrapnel
+    shrapnel_output = telnet.read_until(PYTHON_PROMPT)
+    prompt_index = shrapnel_output.find(PYTHON_PROMPT)
+    banner = shrapnel_output[:prompt_index]
+    sys.stdout.write(banner)
+    sys.stdout.flush()
+
+    prompt = PYTHON_PROMPT
     while True:
-        if not reader.isAlive():
-            print 'giving up'
-            break
         try:
-            line = raw_input()
-        except:
+            line = raw_input(prompt)
+            telnet.write(line + '\r\n')
+            prompt_index, _, response = telnet.expect(PROMPT_REGEXS)
+            prompt = PROMPTS[prompt_index]
+            sys.stdout.write(response[:-len(prompt)])
+            sys.stdout.flush()
+        except EOFError:
             break
-        telnet.write(line + '\r\n')
-    print '\n'
-    os._exit(0)
-#        telnet.close()
+
+    sys.stdout.write(os.linesep)
 
 if __name__ == '__main__':
-    try:
-        host = sys.argv[1]
-    except:
-        pass
-    try:
-        port = int(sys.argv[2])
-    except:
-        pass
-    try:
-        main(host, port)
-    except:
-        pass
-    sys.exit(0)
+    import argparse
+    ap = argparse.ArgumentParser (description = 'shrapnel back door client')
+    ap.add_argument ('addr', help='server address', default='127.0.0.1:23', metavar="(HOST:PORT)|PATH")
+    args = ap.parse_args()
+    if ':' in args.addr:
+        host, port = args.addr.split (':')
+        port = int (port)
+        addr = (host, port)
+    else:
+        addr = args.addr
+    main (addr)
