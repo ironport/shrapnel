f4947f1b348b073c9ff0c8da582584c7091af6da
Sam Rushing <sam-github@rushing.nightmare.com>
2015-02-19 14:54:42 -0800

    move Sync class into coro.log.asn1
    This way log processors don't have to duplicate it.

--- coro/log/asn1.py
+++ coro/log/asn1.py
@@ -3,9 +3,12 @@ from coro.asn1.python import encode
 import coro
 import struct
 
+# four bytes from os.urandom().
+MAGIC = '%\xf1\xbfB'
+
 class Logger:
 
-    magic = '%\xf1\xbfB'
+    magic = MAGIC
 
     def __init__ (self, file):
         self.encode = encode
@@ -21,3 +24,36 @@ class Logger:
             )
         )
         self.file.flush()
+
+
+class Sync:
+
+    magic = MAGIC
+
+    def __init__ (self):
+        self.state = 0
+        self.last = None
+    def feed (self, ch):
+        if ch == self.magic[self.state]:
+            self.state += 1
+            if self.state == 4:
+                return True
+            else:
+                return False
+        else:
+            self.state = 0
+            self.last = ch
+            return False
+    def resync (self, fdin):
+        self.state = 0
+        give_up = 10000
+        i = 0
+        while i < give_up:
+            ch = fdin.read (1)
+            i += 1
+            if ch == '':
+                raise EOFError
+            else:
+                if self.feed (ch):
+                    return
+        raise ValueError ("unable to sync: is this an asn1 log file?")
