4f37916cc11d20b203796d0019031ec0ccbe6255
Sam Rushing <gitsam@rushing.nightmare.com>
2015-09-22 13:32:54 -0700

    note about the firefox magic setting to work with openssl.

--- coro/http/demo/h2_server.py
+++ coro/http/demo/h2_server.py
@@ -3,13 +3,19 @@ import coro.ssl
 import coro.http.spdy
 import coro.backdoor
 
+# note: firefox will not connect to openssl h2 server unless you set
+#  'network.http.spdy.enforce-tls-profile' to false. [see about:config]
+#  chrome works, though.
+# according to https://nghttp2.org/blog/2014/09/15/host-lucid-erlang-http-slash-2-server/
+#  this is because AEAD is missing.
+
 # god how I love openssl.
 cipher_suite = "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!3DES:!MD5:!PSK"
 
 ctx = coro.ssl.new_ctx (
     cert=coro.ssl.x509 (open ('cert/server.crt').read()),
     key=coro.ssl.pkey (open ('cert/server.key').read(), '', True),
-    alpn_protos=['h2'], #, 'http/1.1'],
+    alpn_protos=['h2', 'http/1.1'],
     proto='tlsv2',
     ciphers = cipher_suite,
     dhparam=coro.ssl.dh_param (open ('cert/dhparam.pem').read()),
